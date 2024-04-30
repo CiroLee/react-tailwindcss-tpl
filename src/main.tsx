1753f518495f7700d5a6f8c339adc2543e868a0e
cirolee <ciromail@126.com>
2024-04-30 17:35:16 +0800

    chore: update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { router } from './routes';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
