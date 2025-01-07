9abd4ecf362d5d1f1255bbd6269b37343557709c
CiroLee <ciromail@126.com>
2025-01-07 19:22:44 +0800

    feat: remove StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { router } from './routes';
@@ -6,8 +5,4 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
