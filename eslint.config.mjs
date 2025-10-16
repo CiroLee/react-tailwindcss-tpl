76c8fe0d780ab2ca02e3a0ce5a2e338986c74c06
CiroLee <ciromail@126.com>
2025-10-16 14:39:52 +0800

    chore: update eslint config and upgrade to react19.2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,49 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser, ecmaVersion: 12, sourceType: 'module' } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  reactHooks.configs['recommended-latest'],
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    rules: {
-      'react/react-in-jsx-scope': 0
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended, pluginReact.configs.flat.recommended],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh.configs.vite
     },
-    ignores: ['/node_modules', 'dist'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'max-len': [
+        'error',
+        {
+          code: 200,
+          ignorePattern: '^(\\s*<path\\s+d="[^"]+"|\\s*<svg\\s+[^>]+>)',
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true
+        }
+      ]
+    },
     settings: {
       react: {
         version: 'detect'
       }
     }
   }
-];
+]);
