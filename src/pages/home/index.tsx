3dbe0535fd33f1be2998045a46b8c3ab14a15800
CiroLee <ciromail@126.com>
2025-11-12 14:36:16 +0800

    feat: add a branch to use react compiler

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,24 @@
+import { useState } from 'react';
 export default function Home() {
-  return <h4 className="font-bold text-red-500">react-tailwindcss-tpl</h4>;
+  const [count, setCount] = useState(0);
+  const add = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="space-y-2 p-4">
+      <h1 className="text-2xl font-bold">Parent</h1>
+      <p>count: {count}</p>
+      <button
+        className="mt-2 h-10 cursor-pointer rounded-md border border-gray-500 px-4 active:bg-gray-200"
+        onClick={add}>
+        Add
+      </button>
+      <Child />
+    </div>
+  );
+}
+
+function Child() {
+  console.log('child render');
+  return <div className="w-fit border border-gray-100 p-2">this is child component, please see console</div>;
 }
